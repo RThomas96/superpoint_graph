7380cdec597077cac1fc36db79b54697c0143e7e
Thomas Richard <thomas.richard@liris.cnrs.fr>
2020-11-20 11:11:57 +0100

    Merge csv feature

--- batchTest.py
+++ batchTest.py
@@ -34,6 +34,23 @@ class SppArgs:
     def getProjectPath(self):
         return self.project_path + self.getId()
 
+# Insert all columns from f2 into f1 
+def mergeCSV(fileName1, fileName2):
+    f1 = pd.read_csv(fileName1, sep=';', index_col=False)
+    f2 = pd.read_csv(fileName2, sep=';', index_col=False).tail(1)
+
+    size = len(f1.columns)
+    for i in range(0, len(f2.columns)):
+        f1.insert(size + i, f2.columns[i], f2.to_numpy().flatten()[i])
+    return f1
+
+def mergeCsvPerPair(files1, files2):
+    combi=[]
+    for i in range(0, len(files1)):
+        combi.append(mergeCSV(files1[i], files2[i]))
+    return combi
+        
+
 def main(args):
     parser = argparse.ArgumentParser(description='Batch pipeline')
     parser.add_argument('project_path', help='Project name')
@@ -61,34 +78,28 @@ def main(args):
             os.symlink(args.data_path, "projects/" + sppArgs.getProjectPath() + "/data")
 
             superpointComputation(sppArgs.toList())
-            train([sppArgs.getProjectPath(), "--epoch", "10"])
+            train([sppArgs.getProjectPath(), "--epoch", "20"])
             visualize([sppArgs.getProjectPath(), "test", "LPA3-1", "--outType", "sptgd"])
 
         except FileExistsError:
             print("This project already exist, pass")
 
-    # Concat all csv files
-    sppStatTest = [f +"/reports/sppComputation/statsTest.csv" for f in allProjectsPath]
-    trainStatTest = [f +"/reports/training/statsTest.csv" for f in allProjectsPath]
-
     #import pudb; pudb.set_trace()
 
-    combi=[]
-    for x in range(0, len(sppStatTest)):
+    # Concat all csv files
+    sppStatTest = [f +"/reports/sppComputation/testStats.csv" for f in allProjectsPath]
+    trainStatTest = [f +"/reports/training/testStats.csv" for f in allProjectsPath]
 
-        f1 = pd.read_csv(sppStatTest[x], sep=';', index_col=False)
-        f2 = pd.read_csv(trainStatTest[x], sep=';', index_col=False).tail(1)
+    sppStatValidation = [f +"/reports/sppComputation/validationStats.csv" for f in allProjectsPath]
+    trainStatValidation = [f +"/reports/training/validationStats.csv" for f in allProjectsPath]
 
-        size = len(f1.columns)
-        for i in range(0, len(f2.columns)):
-            f1.insert(size + i, f2.columns[i], f2.to_numpy().flatten()[i])
-        combi.append(f1)
-        
-    final = pd.concat(combi)
-    final.to_csv("projects/" + sppArgs.getProjectPath() + "/../full_spp_testDataset.csv", index=False, na_rep='nan', sep=';')
+    fullReportTest = mergeCsvPerPair(sppStatTest, trainStatTest)
+    fullReportTest = pd.concat(fullReportTest)
+    fullReportTest.to_csv("projects/" + sppArgs.getProjectPath() + "/../full_spp_testDataset.csv", index=False, na_rep='nan', sep=';')
 
-    #combined_csv = pd.concat([pd.read_csv(f +"/reports/sppComputation/statsTraining.csv") for f in allProjectsPath])
-    #combined_csv.to_csv("projects/" + sppArgs.getProjectPath() + "/../full_spp_trainDataset.csv", index=False)
-    
+    fullReportValidation = mergeCsvPerPair(sppStatValidation, trainStatValidation)
+    fullReportValidation = pd.concat(fullReportValidation)
+    fullReportValidation.to_csv("projects/" + sppArgs.getProjectPath() + "/../full_spp_validationDataset.csv", index=False, na_rep='nan', sep=';')
+
 if __name__ == "__main__":
     main(sys.argv[1:])
