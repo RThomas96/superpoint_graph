9301f9228aae930770be32238acfa9bbbe0857d2
Thomas Richard <thomas.richard@liris.cnrs.fr>
2020-11-20 15:48:40 +0100

    Add libs

---
+++ partition/cut-pursuit/build/src/CMakeFiles/cp.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/thomas/Data/Cajun/Data/Evaluation/Methods/superpoint_graph/partition/cut-pursuit/src/cutpursuit.cpp" "/home/thomas/Data/Cajun/Data/Evaluation/Methods/superpoint_graph/partition/cut-pursuit/build/src/CMakeFiles/cp.dir/cutpursuit.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "BOOST_NUMPY_DYN_LINK"
+  "BOOST_PYTHON_DYN_LINK"
+  "OPENMP"
+  "cp_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/../include"
+  "/home/thomas/anaconda3/envs/SPP/include/python3.8"
+  "/home/thomas/anaconda3/envs/SPP/lib/python3.8/site-packages/numpy/core/include"
+  "/home/thomas/anaconda3/envs/SPP/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
